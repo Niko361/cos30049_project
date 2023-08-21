90404d20d74161df4a519740ce50d5c05a5fd89f
Niko361 <alexnikolov684@gmail.com>
2023-08-21 23:42:42 +1000

    week 3 solutions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import BasicGrid from "./grid.js";
+import Blog from "./blog.js";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Blog">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/BasicGrid">BasicGrid</Link>
+            </li>
+          </ul>
+          Week 34
+        </nav>
+        <Routes>
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/BasicGrid" element={<BasicGrid />} />
+        </Routes>
+      </div>
+    );
+  }
 }
-
 export default App;
